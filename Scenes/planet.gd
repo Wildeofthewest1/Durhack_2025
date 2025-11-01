d2945c9ccf5cf9a952fc2fd365c5edf2322559ed
THEREALTOLMO <tolmgaming@gmail.com>
2025-11-01 19:07:39 +0000

    preparing to merge with main

--- Scenes/planet.gd
+++ Scenes/planet.gd
@@ -4,22 +4,28 @@ extends CharacterBody2D
 @export var initial_speed = 0
 @export var initial_direction = Vector2.ZERO 
 
+
 @export var autoorbit = false
-@export var orbital_parent: CharacterBody2D
+@export var parent_star: CharacterBody2D #Add the host star of the system
+@export var parent_planet: CharacterBody2D #Add planetary moons 
+
+var gravity_scale = 1e3
+
 func _ready():
 	if not autoorbit:
 		velocity = initial_speed * initial_direction
 	if autoorbit and not is_in_group("Star"):
 		#times 60 because of the physics framerate
-		var speed = (60 * orbital_parent.mass * 1e4/(position - orbital_parent.position).length())**(0.5)
+		var speed = (60 * parent_star.mass * gravity_scale/(position - parent_star.position).length())**(0.5)
 		print(speed)
-		var direction = (position - orbital_parent.position).normalized()
+		var direction = (position - parent_star.position).normalized()
 		velocity = speed * direction.orthogonal()
 
 
 func _process(_delta: float) -> void:
 	#print(name, " ", (position - orbital_parent.position).length())
-	#if not is_in_group("Star"):
+	if not is_in_group("Star"):
+		print((position - parent_star.position).length())
 		##print((orbital_parent.mass 
 				##* 1e4 * 30/(position - orbital_parent.position).length())**(0.5))
 		#pass
@@ -34,4 +40,4 @@ func force_g():
 		if i != get_index():
 			var direction_g = position - planets[i].global_position
 			velocity -= (direction_g.normalized()
-							* planets[i].mass * 1e4/direction_g.length()**2)
+							* planets[i].mass * gravity_scale/direction_g.length()**2)
