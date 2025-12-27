a684352ec248547be8d57e6920293f2ec0f4608f
mekindog <mekindog@gmail.com>
2025-12-27 12:00:04 +0300

    scrap and railgun hit effect visual update

--- Scenes/particles/explosion_2.gd
+++ Scenes/particles/explosion_2.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
 var emitting := false
-@onready var particle1 := $particle1
-@onready var particle2 := $particle2
+@onready var particle1 := $Forward
+@onready var particle2 := $Backward
 @onready var particle3 := $explosion
+@onready var particle4 := $Forward2
+
 
 func _physics_process(_delta: float) -> void:
 	if emitting == true:
 		particle1.emitting = true
 		particle2.emitting = true
 		particle3.emitting = true
+		particle4.emitting = true
 		
 		emitting = false
 		var timer := Timer.new()
